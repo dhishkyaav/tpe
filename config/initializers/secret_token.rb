0abfb97ad635a9aa0f195f0b742fc5c6da7057d0
Akshay Dhavle <akshay.dhavle@gmail.com>
2011-01-04 14:33:37 +0530

    [AD] repository checked in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tpe::Application.config.secret_token = '924e715a0ee78031291cc9cf22c0758b06d0bdd6ed367a6727c6ef7fa3fe853558e5ba4415e34008d4dee520b494c8f68684354f457b96d5d8070bc5e523c6c8'
